d4e1b48926a09f5c766f14259914005a861e06d1
Oliver <draper87@gmail.com>
2020-06-25 16:02:13 +0200

    4

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 $(document).ready(function() {
 
+  // mi creo una funzione che automatizzi la creazione della griglia 6x6
   function creaBox() {
     numeroBox = 36;
     for (var i = 0; i < numeroBox ; i++) {
@@ -11,8 +12,10 @@ $(document).ready(function() {
     }
   }
 
+  // richiamo la funzione
   creaBox();
 
+  // quando clicco su un riquadro voglio che venga effettuata una chiamata ajax che restituisca un numero da 1 a 9
   $('.box').click(function() {
     var clicked = this;
 
@@ -23,10 +26,12 @@ $(document).ready(function() {
       success: function(data) {
         var numeroRandom = data.response;
         if (numeroRandom > 5) {
+          // in caso di numero superiore a 5 il riquadro diventa verde
           $(clicked).removeClass('green yellow');
           $(clicked).text(numeroRandom).addClass('green');
         }
         else {
+          // in caso di numero inferiore o uguale a 5 il riquadro diventa giallo
           $(clicked).removeClass('green yellow');
           $(clicked).text(numeroRandom).addClass('yellow');
         }
